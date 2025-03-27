d2fcc44da33f912d6855ac2d30dcf61e31874cc1
slingshotvfx <146885925+slingshotvfx@users.noreply.github.com>
2025-03-27 12:57:03 -0700

    feat: add "load config from file" menu item

--- src/slingshot_autoloader.py
+++ src/slingshot_autoloader.py
@@ -15,7 +15,6 @@ import PyOpenColorIO as OCIO
 from rv import commands, extra_commands, rvtypes
 from rv_menu_schema import MenuItem
 from slingshot_autoloader_config import (
-    get_config_path,
     get_ocio_config,
     load_config_from_file,
     load_or_create_config,
@@ -95,7 +94,7 @@ class SlingshotAutoLoaderMode(rvtypes.MinorMode):
         ]
 
         self.init(
-            "rv-auto-loader",
+            "slingshot-autoloader",
             init_bindings,
             None,
             menu=self.give_menu(),
@@ -172,8 +171,8 @@ class SlingshotAutoLoaderMode(rvtypes.MinorMode):
                             ).tuple(),
                             MenuItem("_").tuple(),
                             MenuItem(
-                                label=f"To configure these, edit {get_config_path().name}",
-                                stateHook=lambda: commands.DisabledMenuState,
+                                label="Load config from file...",
+                                actionHook=self.load_config_from_file,
                             ).tuple(),
                         ],
                     ),
@@ -224,6 +223,25 @@ class SlingshotAutoLoaderMode(rvtypes.MinorMode):
                 logger.setLevel(logging.DEBUG if new_setting else logging.INFO)
 
         return _toggle
+
+    def load_config_from_file(self, event: "Event"):
+        try:
+            if cfg_path := commands.openFileDialog(
+                True, False, False, "cfg|Autoloader Config Files (*.cfg)", None
+            ):
+                self.config = load_config_from_file(Path(cfg_path[0]))
+                commands.defineModeMenu("slingshot-autoloader", self.give_menu(), True)
+        except Exception as e:
+            logger.warning(f"Error loading config: {e}")
+            commands.alertPanel(
+                True,
+                commands.ErrorAlert,
+                "Error loading config",
+                f"{e}\n\nCheck your config file and try again.",
+                "Okay",
+                None,
+                None,
+            )
 
     def _find_file(self, source_path: Path, search_path: str) -> Path | None:
         if matches := re.search(
